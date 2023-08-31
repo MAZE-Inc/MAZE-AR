d3085f16a032d927f09659e34b260f090ed1695c
MAZE Choiji <113877093+MAZE-Choiji@users.noreply.github.com>
2023-08-31 19:53:25 +0900

    [UPDATE] Read me 수정

--- MAZEAR/App.jsx
+++ MAZEAR/App.jsx
@@ -1,107 +1,128 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react'
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
-} from 'react-native';
+  Platform,
+  TouchableHighlight,
+  Text,
+} from 'react-native'
+import { ArViewerView } from 'react-native-ar-viewer'
+import RNFS from 'react-native-fs'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const AR = () => {
+  const [localModelPath, setLocalModelPath] = useState(null)
+  const [showArView, setShowArView] = useState(true)
+  const ref = useRef()
 
+  const loadPath = async () => {
+    const modelSrc =
+      Platform.OS === 'android'
+        ? 'https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.glb?raw=true'
+        : 'https://developer.apple.com/augmented-reality/quick-look/models/cupandsaucer/cup_saucer_set.usdz?raw=true'
+        // : 'https://github.com/MAZE-Inc/A-MAZE-MOBILE-APP/blob/choiji/app/assets/cup.usdz?raw=true'
+    const modelPath = `${ RNFS.DocumentDirectoryPath }/model.${ Platform.OS === 'android' ? 'glb' : 'usdz' }`
+    const exists = await RNFS.exists(modelPath)
+    console.log(312312312, modelPath)
+    console.log(3123, exists)
+    if(!exists) {
+      await RNFS.downloadFile({
+        fromUrl: modelSrc,
+        toFile: modelPath,
+        discretionary: true,
+        cacheable: true,
+      }).promise
+    }
 
-function Section({children, title}) {
-  const isDarkMode = useColorScheme() === 'dark';
+    setLocalModelPath(modelPath)
+  }
+
+  useEffect(() => {
+    // console.log('load????')
+    loadPath()
+  }, [])
+
+  // const takeSnapshot = () => {
+  //   ref.current?.takeScreenshot().then(async (base64Image) => {
+  //     const date = new Date()
+  //     const filePath = `${
+  //       RNFS.CachesDirectoryPath
+  //     }/arscreenshot-${ date.getFullYear() }-${ date.getMonth() }-${ date.getDay() }-${ date.getHours() }-${ date.getMinutes() }-${ date.getSeconds() }.jpg`
+  //     await RNFS.writeFile(filePath, base64Image, 'base64')
+  //     console.log('Screenshot written to ' + filePath)
+  //   })
+  // }
+
+  // const reset = () => {
+  //   ref.current?.reset()
+  // }
+
+  // const rotate = () => {
+  //   ref.current?.rotate(0, 25, 0)
+  // }
+
+  // const mountUnMount = () => setShowArView(!showArView)
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={ styles.container }>
+      { localModelPath && (
+        <ArViewerView
+          model={ localModelPath }
+          style={ styles.arView }
+          lightEstimation
+          disableInstantPlacement
+          manageDepth
+          allowRotate
+          allowScale
+          allowTranslate
+          onStarted={ () => console.log('started') }
+          onEnded={ () => console.log('ended') }
+          onModelPlaced={ () => console.log('model displayed') }
+          onModelRemoved={ () => console.log('model not visible anymore') }
+          ref={ ref }
+        />
+      ) }
+      {/* <View style={ styles.footer }>
+        <TouchableHighlight onPress={ takeSnapshot } style={ styles.button }>
+          <Text>Take Snapshot</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={ mountUnMount } style={ styles.button }>
+          <Text>{ showArView ? 'Unmount' : 'Mount' }</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={ reset } style={ styles.button }>
+          <Text>Reset</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={ rotate } style={ styles.button }>
+          <Text>Rotate</Text>
+        </TouchableHighlight>
+      </View> */}
     </View>
-  );
-}
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  arView: {
+    flex: 2,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    backgroundColor: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 5,
   },
-});
+})
 
-export default App;
+export default AR
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MAZE-AR
+
+AR Core와 AR Kit 이용하여 AR View를 보여주게 됩니다.AR View를 그리는데 필요한 파일이 Android와 Ios가 각각 달라 해당 파일을 서버로부터 전달받아 App에서 render하게 됩니다.
+
+AR View를 render할 때 두가지 이상의 파일을 한번에 render하려고 하면 render속도와 성능이 저하되기 때문에 UX적으로 좋지 않습니다. 그렇기 때문에 저희는 메뉴의 갯수에 따라 동적으로 AR View를 그리게 되고, 하나의 화면에 한 가지의 AR을 보여주고 다음 버튼을 통해 다른 AR View를 보여주게 됩니다.
